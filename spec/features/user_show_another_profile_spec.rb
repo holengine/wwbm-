7d33dd1c8cb3638c7acca172338beec034fced39
hol3ng <savinyhal@gmail.com>
2023-01-23 16:29:11 +0300

    fix cs

--- spec/features/user_show_another_profile_spec.rb
+++ spec/features/user_show_another_profile_spec.rb
@@ -5,15 +5,19 @@ RSpec.feature 'USER show another user profile', type: :feature do
   let(:another_user) { create :user, name: 'Пользователь №2' }
 
   let!(:games) do
-    create(:game, user_id: user.id,
+    create(:game,
+           user_id: user.id,
            created_at: Time.parse('2023-01-23, 14:00 UTC'),
-           current_level: 5
+           current_level: 3904,
+           prize: 55
     )
 
-    create(:game, user_id: user.id,
+    create(:game,
+           user_id: user.id,
            created_at: Time.parse('2023-01-23, 13:00 UTC'),
            finished_at: Time.parse('2023-01-23, 13:10 UTC'),
            is_failed: true,
+           current_level: 22232,
            prize: 1000
     )
   end
@@ -30,13 +34,13 @@ RSpec.feature 'USER show another user profile', type: :feature do
     expect(page).to have_content('в процессе')
     expect(page).to have_content('проигрыш')
 
-    expect(page).to have_content('5')
-    expect(page).to have_content('0')
+    expect(page).to have_content('3904')
+    expect(page).to have_content('22232')
 
     expect(page).to have_content('23 янв., 14:00')
     expect(page).to have_content('23 янв., 13:00')
 
-    expect(page).to have_content('0 ₽')
+    expect(page).to have_content('55 ₽')
     expect(page).to have_content('1 000 ₽')
 
     expect(page).to have_content('50/50')
