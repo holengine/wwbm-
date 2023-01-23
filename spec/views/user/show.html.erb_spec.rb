0f310ba41cf6086adc753d0c885bb98d9bc8d504
hol3ng <savinyhal@gmail.com>
2023-01-23 12:48:12 +0300

    tests show.html.erb_spec

---
+++ spec/views/user/show.html.erb_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe "users/show", type: :view do
+  let(:user) { build_stubbed(:user, name: "User", balance: 1000) }
+
+  before() do
+    assign(:user, user)
+    assign(:games, [nil])
+
+    allow(view).to receive(:current_user).and_return(user)
+
+    stub_template "users/_game.html.erb" => "template game"
+
+    render
+  end
+
+  it 'renders user name' do
+    expect(rendered).to match 'User'
+  end
+
+  it 'renders button change password' do
+    expect(rendered).to match link_to 'Сменить имя и пароль', edit_user_registration_path(user)
+  end
+
+  it 'renders games' do
+    expect(rendered).to match 'template game'
+  end
+end
